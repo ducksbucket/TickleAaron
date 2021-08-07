ab42ac01199ba2acd1aa25e99320aa2d61e71b10
Douglas Childs <github@childsmail.net>
2021-08-07 15:49:51 -0500

    Initial commit

---
+++ app/src/main/java/net/ducksbucket/tickleaaron/MainActivity.java
@@ -0,0 +1,28 @@
+package net.ducksbucket.tickleaaron;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final String EXTRA_MESSAGE = "net.ducksbucket.tickleaaron.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Select button */
+    public void handleButtonSelect(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
